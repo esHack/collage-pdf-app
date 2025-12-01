705e361f1fed4ea2fba8a709eee98406c4585b17
Eswar Yaga <eswaryaga@gmail.com>
2025-12-01 23:32:28 +0100

    Fix executablePath - add parentheses to call function

--- api/generate-pdf.js
+++ api/generate-pdf.js
@@ -98,7 +98,7 @@ module.exports = async (req, res) => {
     browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
+      executablePath: await chromium.executablePath(),
       headless: chromium.headless,
     });
     
